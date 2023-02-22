6881c60c8864c3e34d43663511ce50afca5615b8
AdikaDS <adika.dwi.saputra@mail.ugm.ac.id>
2023-02-22 15:21:01 +0700

    commit dinamis

--- app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
+++ app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
@@ -16,33 +16,13 @@ public class SimpleFragment extends Fragment {
 
     private static final int YES = 0;
     private static final int NO = 1;
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public SimpleFragment() {
         // Required empty public constructor
     }
 
-    public static SimpleFragment newInstance(String param1, String param2) {
-        SimpleFragment fragment = new SimpleFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public static SimpleFragment newInstance() {
+        return new SimpleFragment();
     }
 
     @Override
